75913b619922fda5cbc90578130ffb7aede6bbb9
Vasiliy <vasily.grebenuk@gmail.com>
2018-12-03 23:19:16 +0300

    Vertu volume

---
+++ Vertu.java
@@ -0,0 +1,7 @@
+public class Vertu {
+    public static void main(String[]args){
+        float radius = 6.0f, height = 10.0f ;
+        float volume = 3.14f * radius * radius * height;
+        System.out.println("цилиндр объемом" + volume);
+    }
+}
